3b8d5c9e05c69076a705fb7afe1290e4a331d454
eniobiondic <enio.biondic@undabot.com>
2025-01-07 13:03:50 +0100

    chore: format existing code with new lint rules

--- composeApp/src/iosMain/kotlin/com/biondic/rssreader/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/biondic/rssreader/MainViewController.kt
@@ -1,10 +1,12 @@
 package com.biondic.rssreader
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.biondic.rssreader.core.di.platformModule
+import com.biondic.rssreader.core.di.sharedModule
 import org.koin.core.context.startKoin
 
 fun MainViewController() = ComposeUIViewController(
-    configure = { startKoin { modules() } }
+    configure = { startKoin { modules(sharedModule(), platformModule()) } },
 ) {
     AppContainer()
 }
--- composeApp/src/nativeMain/kotlin/com/biondic/rssreader/core/di/PlatformModule.native.kt
+++ composeApp/src/nativeMain/kotlin/com/biondic/rssreader/core/di/PlatformModule.native.kt
@@ -4,5 +4,4 @@ import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
-
 }
