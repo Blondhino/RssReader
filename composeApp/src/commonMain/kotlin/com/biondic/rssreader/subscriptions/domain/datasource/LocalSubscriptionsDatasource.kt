86cc93edc55d7ed9a3bc5ebe79c9af3334a67ae3
eniobiondic <enio.biondic@undabot.com>
2025-01-08 15:49:35 +0100

    chore: define SubscriptionRepository, LocalSubscriptionsDatasource and RemoteSubscriptionsDatasource

---
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/subscriptions/domain/datasource/LocalSubscriptionsDatasource.kt
@@ -0,0 +1,13 @@
+package com.biondic.rssreader.subscriptions.domain.datasource
+
+import arrow.core.Either
+import com.biondic.rssreader.core.model.DatabaseError
+import com.biondic.rssreader.subscriptions.domain.model.Subscription
+
+interface LocalSubscriptionsDatasource {
+    fun getSubscriptions(): Either<DatabaseError, List<Subscription>>
+    fun getSingleSubscription(url: String): Either<DatabaseError, List<Subscription>>
+    fun saveSubscription(subscription: Subscription): Either<DatabaseError, Unit>
+    fun deleteSubscription(url: String): Either<DatabaseError, Unit>
+    fun updateSubscription(subscription: Subscription): Either<DatabaseError, Unit>
+}
