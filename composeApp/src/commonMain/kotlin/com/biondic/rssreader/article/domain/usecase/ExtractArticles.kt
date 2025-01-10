7fa5e9acab5fed14c16645e1ea554d9ee42475fe
eniobiondic <enio.biondic@undabot.com>
2025-01-10 14:55:11 +0100

    feat: define use-cases to support business logic related to articles feature

---
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/article/domain/usecase/ExtractArticles.kt
@@ -0,0 +1,39 @@
+package com.biondic.rssreader.article.domain.usecase
+
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.raise.either
+import arrow.core.right
+import com.biondic.rssreader.article.domain.model.Article
+import com.biondic.rssreader.article.domain.model.ArticleData
+import com.biondic.rssreader.article.domain.model.ArticleData.Local
+import com.biondic.rssreader.article.domain.model.ArticleData.Remote
+import com.biondic.rssreader.core.model.RepositoryError
+
+class ExtractArticles {
+    private val localItems: MutableList<Article> = mutableListOf()
+    operator fun invoke(
+        articleData: ArticleData,
+    ): Either<RepositoryError, List<Article>> = when (articleData) {
+        is Remote -> updateWithRemoteDataOrReturnLocal(articleData).right()
+        is Local -> getArticlesOrErrorFromLocalData(articleData)
+    }
+
+    private fun getArticlesOrErrorFromLocalData(data: Local) = either {
+        data.results.onRight {
+            localItems.clear()
+            localItems.addAll(it)
+        }.bind()
+    }
+
+    private fun updateWithRemoteDataOrReturnLocal(remoteData: Remote): List<Article> =
+        when (remoteData.results) {
+            is Left -> localItems
+            is Right -> {
+                localItems.clear()
+                localItems.addAll(remoteData.results.value)
+                remoteData.results.value
+            }
+        }
+}
---
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/article/domain/usecase/GetArticles.kt
@@ -0,0 +1,27 @@
+package com.biondic.rssreader.article.domain.usecase
+
+import arrow.core.Either
+import com.biondic.rssreader.article.domain.model.Article
+import com.biondic.rssreader.article.domain.model.ArticleData.Remote
+import com.biondic.rssreader.article.domain.repo.ArticleRepository
+import com.biondic.rssreader.core.model.RefreshStrategy
+import com.biondic.rssreader.core.model.RefreshStrategy.SyncWithRemote
+import com.biondic.rssreader.core.model.RepositoryError
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+
+class GetArticles(
+    private val repository: ArticleRepository,
+    private val extractArticles: ExtractArticles,
+) {
+    operator fun invoke(
+        subscriptionUrl: String,
+        refreshStrategy: RefreshStrategy,
+        refreshingCompleted: () -> Unit,
+    ): Flow<Either<RepositoryError, List<Article>>> = repository.getArticlesForSubscription(
+        shouldRefresh = refreshStrategy is SyncWithRemote,
+        url = subscriptionUrl,
+    ).onEach { if (it is Remote) refreshingCompleted() }
+        .map(extractArticles::invoke)
+}
