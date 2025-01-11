b297c062bfe2c32d683c1ebb8a2487ab1785fdbf
eniobiondic <enio.biondic@undabot.com>
2025-01-11 21:33:19 +0100

    chore: Update RssString to convert HTML text into plain text

--- composeApp/src/commonMain/kotlin/com/biondic/rssreader/core/model/rss/RssString.kt
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/core/model/rss/RssString.kt
@@ -4,6 +4,7 @@ import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 import com.biondic.rssreader.core.utils.decodeHtmlEntities
+import com.biondic.rssreader.core.utils.extractPlainTextFromHtml
 import kotlin.jvm.JvmInline
 
 @JvmInline
@@ -59,12 +60,14 @@ value class RssString private constructor(private val value: String) {
         .substringBefore(RSS_TITLE_END)
         .substringAfter(RSS_CDATA_START)
         .substringBefore(RSS_CDATA_END)
+        .extractPlainTextFromHtml()
 
     private fun String.extractDescription() = this
         .substringAfter(RSS_DESCRIPTION_START, OR_EMPTY)
         .substringBefore(RSS_DESCRIPTION_END)
         .substringAfter(RSS_CDATA_START)
         .substringBefore(RSS_CDATA_END)
+        .extractPlainTextFromHtml()
 
     private fun String.extractFeedImageUrl() = this
         .substringBefore(RSS_ITEM_START)
---
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/core/utils/ExtractPlainTextFromHtml.kt
@@ -0,0 +1,18 @@
+package com.biondic.rssreader.core.utils
+
+fun String.extractPlainTextFromHtml(): String {
+    val noTags = replace(Regex("<[^>]*>"), "")
+    val decodedText = noTags.replace(Regex("&#(\\d+);")) { match ->
+        val charCode = match.groupValues[1].toIntOrNull()
+        charCode?.toChar()?.toString() ?: match.value
+    }
+    return decodedText
+        .replace("&nbsp;", " ")
+        .replace("&amp;", "&")
+        .replace("&lt;", "<")
+        .replace("&gt;", ">")
+        .replace("&quot;", "\"")
+        .replace("&#8216;", "‘")
+        .replace("&#8217;", "’")
+        .replace("&#8230;", "…")
+}
