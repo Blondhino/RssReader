5fb8962ccc66fda54688aced8aa7ea09cfb5ab37
eniobiondic <enio.biondic@undabot.com>
2025-01-11 21:35:51 +0100

    chore: remove Feed package

--- composeApp/src/commonMain/kotlin/com/biondic/rssreader/feed/di/FeedModule.kt
+++
@@ -1,9 +0,0 @@
-package com.biondic.rssreader.feed.di
-
-import com.biondic.rssreader.feed.ui.FeedViewModel
-import org.koin.core.module.dsl.factoryOf
-import org.koin.dsl.module
-
-fun feedModule() = module {
-    factoryOf(::FeedViewModel)
-}
--- composeApp/src/commonMain/kotlin/com/biondic/rssreader/feed/ui/FeedViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.biondic.rssreader.feed.ui
-
-import cafe.adriel.voyager.core.model.ScreenModel
-
-class FeedViewModel : ScreenModel {
-    fun getScreenTitle() = "This is the Feed Screen"
-}
