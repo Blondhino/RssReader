e9086a4d23c627eee2544815dc83f4d87f207817
eniobiondic <enio.biondic@undabot.com>
2025-01-14 10:24:26 +0100

    chore: address lint

--- composeApp/src/commonMain/kotlin/com/biondic/rssreader/core/ui/components/ScreenTopBar.kt
+++ composeApp/src/commonMain/kotlin/com/biondic/rssreader/core/ui/components/ScreenTopBar.kt
@@ -6,15 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
@@ -36,7 +35,7 @@ fun ScreenTopBar(
                 .align(Alignment.CenterStart),
             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
             contentDescription = null,
-            tint = MaterialTheme.colorScheme.surface
+            tint = MaterialTheme.colorScheme.surface,
         )
 
         Text(
@@ -44,7 +43,7 @@ fun ScreenTopBar(
             text = title,
             maxLines = 1,
             overflow = TextOverflow.Clip,
-            color = MaterialTheme.colorScheme.surface
+            color = MaterialTheme.colorScheme.surface,
         )
     }
 }
